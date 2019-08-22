cfabcbdad5d431bfa9a57c64d3d3c976d3b69d0a
Magnus Marthinsen <magmar@online.no>
2019-08-22 10:04:14 +0200

    removed warnings from Knowledge

--- src/hvl/projectparmorel/ml/Knowledge.java
+++ src/hvl/projectparmorel/ml/Knowledge.java
@@ -4,7 +4,6 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.NotSerializableException;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
