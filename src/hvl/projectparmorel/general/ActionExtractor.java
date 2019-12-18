5383e5412d34c351a823c78d0896244e786debd3
Magnus Marthinsen <magmar@online.no>
2019-12-18 21:53:22 +0100

    updated JavaDoc with hints to other potential developers

--- src/hvl/projectparmorel/general/ActionExtractor.java
+++ src/hvl/projectparmorel/general/ActionExtractor.java
@@ -7,8 +7,10 @@ public interface ActionExtractor {
 	/**
 	 * Extract all the actions that has the potential to solve the specified errors.
 	 * 
+	 * Optimally, only actions that results in a change to the model are included in the list to narrow the search space.
+	 * 
 	 * @param errors
-	 * @return
+	 * @return a list of actions
 	 */
 	public List<Action> extractActionsFor(List<Error> errors);
 }
