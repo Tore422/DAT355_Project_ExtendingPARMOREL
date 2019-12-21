9ab61208c9516b2ef5257806aef2646ae628959e
Magnus Marthinsen <magmar@online.no>
2019-12-21 18:44:06 +0100

    created a method in solution that can reward that specific solution. I needed the reward-caclulator to do it, so now the Solution can hold a reference to the calculator.

--- src/hvl/projectparmorel/modelrepair/Solution.java
+++ src/hvl/projectparmorel/modelrepair/Solution.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import hvl.projectparmorel.general.AppliedAction;
+import hvl.projectparmorel.reward.RewardCalculator;
 
 /**
  * @author Magnus Marthinsen
@@ -18,6 +19,7 @@ public class Solution implements Comparable<Solution> {
 	private double weight;
 	private File model;
 	private File original;
+	private RewardCalculator rewardCalculator;
 
 	public Solution() {
 		super();
@@ -83,6 +85,28 @@ public class Solution implements Comparable<Solution> {
 		return Double.compare(weight, solution.getWeight());
 	}
 
+	
+	/**
+	 * Awards the solution by boosting all the actions taken
+	 */
+	public void award() {
+		System.out.println("Awarding solution. The following actions are affected:");
+		for(AppliedAction appliedAction : sequence) {
+			System.out.println("Error: " + appliedAction.getError().getCode());
+			System.out.println("\t Context: " + appliedAction.getAction().getContextId());
+			System.out.println("\t Action: " + appliedAction.getAction().getCode());
+			System.out.println("\t \t Old weight: " + appliedAction.getAction().getWeight());
+		}
+		rewardCalculator.rewardSolution(this, -1, true);
+		System.out.println("After award this is the result:");
+		for(AppliedAction appliedAction : sequence) {
+			System.out.println("Error: " + appliedAction.getError().getCode());
+			System.out.println("\t Context: " + appliedAction.getAction().getContextId());
+			System.out.println("\t Action: " + appliedAction.getAction().getCode());
+			System.out.println("\t \t New weight: " + appliedAction.getAction().getWeight());
+		}
+	}
+
 	/**
 	 * Set the original model
 	 * 
@@ -98,5 +122,14 @@ public class Solution implements Comparable<Solution> {
 	 */
 	public File getOriginal() {
 		return original;
+	}
+
+	/**
+	 * Set the reward calculator used to generate the solution
+	 * 
+	 * @param rewardCalculator
+	 */
+	public void setRewardCalculator(RewardCalculator rewardCalculator) {
+		this.rewardCalculator = rewardCalculator;
 	}
 }
