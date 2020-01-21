a491ba8b12a22dfcc7572a4a839f79c493dd1255
Magnus Marthinsen <magmar@online.no>
2020-01-21 09:19:16 +0100

    tried to calculate distance, but always get 0, even though the comparison has differences

--- src/hvl/projectparmorel/ecore/EcoreSolution.java
+++ src/hvl/projectparmorel/ecore/EcoreSolution.java
@@ -1,12 +1,16 @@
 package hvl.projectparmorel.ecore;
 
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.compare.Comparison;
 import org.eclipse.emf.compare.Diff;
 import org.eclipse.emf.compare.EMFCompare;
+import org.eclipse.emf.compare.match.eobject.CachingDistance;
+import org.eclipse.emf.compare.match.eobject.EditionDistance;
 import org.eclipse.emf.compare.scope.DefaultComparisonScope;
 import org.eclipse.emf.compare.scope.IComparisonScope;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
@@ -32,6 +36,30 @@ public class EcoreSolution extends Solution {
 		IComparisonScope scope = new DefaultComparisonScope(originalResourceSet, modelResourceSet, null);
 		Comparison comparison = EMFCompare.builder().build().compare(scope);
 		EList<Diff> diff = comparison.getDifferences();
-		return diff.size();
+		
+		Resource orignalResource = originalResourceSet.getResource(originalUri, false);
+		Resource modelResource = modelResourceSet.getResource(modelUri, false);
+		
+		EditionDistance distanceCalculator = new EditionDistance(); //EditionDistance.builder().build();
+//		CachingDistance cachingDistanceCalculator = new CachingDistance();
+		
+		double totalDistance = 0;
+		System.out.println("Calculating distance. Number of differences: " + diff.size());
+		System.out.println("Number of contents:" + orignalResource.getContents().size());
+		System.out.println("Number of new contents:" + modelResource.getContents().size());
+		for(int i = 0; i < orignalResource.getContents().size(); i++) {
+			EObject originalObject = orignalResource.getContents().get(i);
+			EObject newObject = modelResource.getContents().get(i);
+			
+			System.out.print("Are identic: ");
+			System.out.println(Boolean.toString(distanceCalculator.areIdentic(comparison, originalObject, newObject)));
+			
+			totalDistance += distanceCalculator.distance(comparison, originalObject, newObject);
+			System.out.println("New total distance: " + totalDistance);
+		}
+		System.out.println();
+		return totalDistance; // distanceCalculator.distance(comparison, originalModel, newModel);
+		
+//		return diff.size();
 	}
 }
