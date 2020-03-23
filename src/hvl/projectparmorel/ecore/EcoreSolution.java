6017ab9813fdcd6d9ff3e59a215c8d9337066e39
Magnus Marthinsen <magmar@online.no>
2020-03-23 10:37:06 +0100

    The console no longer displays the stack trace, but rather logs the distance or the error.

--- src/hvl/projectparmorel/ecore/EcoreSolution.java
+++ src/hvl/projectparmorel/ecore/EcoreSolution.java
@@ -1,22 +1,29 @@
 package hvl.projectparmorel.ecore;
 
+import java.util.logging.Logger;
+
 import hvl.projectparmorel.modelrepair.Solution;
 import it.cs.gssi.similaritymetamodels.EComparator;
 
 public class EcoreSolution extends Solution {
-	
+
+	private Logger logger;
+
 	public EcoreSolution() {
 		super();
+		logger = Logger.getLogger("MyLog");
 	}
 
 	@Override
 	public double calculateDistanceFromOriginal() {
 		double distance = -1;
-		EComparator comparator = new EComparator(getOriginal().getAbsolutePath(), getModel().getAbsolutePath()); 
+		EComparator comparator = new EComparator(getOriginal().getAbsolutePath(), getModel().getAbsolutePath());
 		try {
+			logger.info("Calculating distance between " + getOriginal().getName() + " and " + getModel().getName());
 			distance = comparator.execute(getOriginal().getAbsolutePath(), getModel().getAbsolutePath());
+			logger.info("Calculated the distance between the models to " + distance);
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.warning("Could not calculate the distance between the models.");
 		}
 		return distance;
 	}
